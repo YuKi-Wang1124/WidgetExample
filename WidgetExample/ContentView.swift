00a96859b9c82337059155d99dfaec0b6253f707
mandy <mandy@newpos.tw>
2024-10-11 16:43:13 +0800

    [chore] master init

---
+++ WidgetExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetExample
+//
+//  Created by 王昱淇 on 2024/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetExample/WidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetExampleApp.swift
+//  WidgetExample
+//
+//  Created by 王昱淇 on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct WidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
